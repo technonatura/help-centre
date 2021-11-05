114b3ec527092ce39ce3d10bf9f7a822732aa200
aldhanekaa <aldhanekadev@gmail.com>
2021-11-05 23:08:34 +0700

    add: initial

---
+++ src/@mlent/gatsby-theme-help-center/themes/colors.ts
@@ -0,0 +1,32 @@
+export const COLORS = {
+  primary: {
+    light: "#5BE584",
+    main: "#00AB55",
+    dark: "#007B55",
+    contrastText: "white",
+  },
+  secondary: {
+    light: "#5BE584",
+    dark: "#5BE584",
+    main: "#5BE584",
+    contrastText: "white",
+  },
+  success: {
+    light: "#AAF27F",
+    dark: "#229A16",
+    main: "#54D62C",
+    contrastText: "white",
+  },
+  pending: {
+    light: "#F5E18C",
+    dark: "#F6CC1B",
+    main: "#F6CC1B",
+    contrastText: "white",
+  },
+  error: {
+    light: "#FFA48D",
+    dark: "#DB4D4D",
+    main: "#DB4D4D",
+    contrastText: "white",
+  },
+};
